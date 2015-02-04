096d0834743ee2d93edc084dc154d56ff61b7260
Carlos García Trujillo <cgarciagl@gmail.com>
2015-02-03 18:36:21 -0600

    process helper Update for Windows

--- application/third_party/yupii/helpers/process_helper.php
+++ application/third_party/yupii/helpers/process_helper.php
@@ -8,6 +8,12 @@ function timeStart() {
     $starttime = $mtime;
 }
 
+function fecho($string) {
+    echo $string;
+    @ob_flush();
+    flush();
+}
+
 function timeEnd() {
     global $starttime;
     $mtime = microtime();
@@ -20,31 +26,37 @@ function processStart() {
     $ci = &get_instance();
     set_time_limit(0);
     ini_set('memory_limit', '-1');
+    @ini_set('zlib.output_compression', 0);
+    @ini_set('implicit_flush', 1);
+    @ob_end_clean();
+
     $ci->db->save_queries = false;
     ob_implicit_flush(true);
-    echo $ci->load->view('process/progress', null, TRUE);
     timeStart();
+    fecho($ci->load->view('process/progress', null, TRUE));
 }
 
 function setProgress($porcentaje) {
     $porcentaje = ceil($porcentaje);
-    echo "<script type='text/javascript'> $('#pbar').text('$porcentaje %').css('width','$porcentaje%');</script>";
+    fecho("<script type='text/javascript'> $('#pbar').text('$porcentaje %').css('width','$porcentaje%');</script>");
 }
 
 function setProgressText($texto) {
-    echo "<script type='text/javascript'> $('#textopbar').html('$texto');</script>";
+    fecho("<script type='text/javascript'> $('#textopbar').html('$texto');</script>");
+
 }
 
 function setProgressTitle($texto) {
-    echo "<script type='text/javascript'> $('#textotitulo').text('$texto');</script>";
+    fecho("<script type='text/javascript'> $('#textotitulo').text('$texto');</script>");
 }
 
 function endProcess() {
     setProgressText('');
-    echo "<script type='text/javascript'> $('#girando').removeClass('fa-spin');</script>";
+    fecho("<script type='text/javascript'> $('#girando').removeClass('fa-spin');</script>");
     $tiempo = round(timeEnd(), 5);
-    echo "<hr> <small>Tiempo del proceso: $tiempo segs.</small>";
-    echo '</div></body>';
+    fecho("<hr> <small>Tiempo del proceso: $tiempo segs.</small>");
+    fecho('</div></body>');
+
 }
 
 function percentOf($i, $total) {
@@ -52,23 +64,11 @@ function percentOf($i, $total) {
 }
 
 function redirectTo($ruta, $segundos = 0) {
-    echo "<script type='text/javascript'> setTimeout(function () {
+    fecho("<script type='text/javascript'> setTimeout(function () {
         window.location.href = base_url + 'index.php/$ruta';
-    }, $segundos*1000);</script>";
+    }, $segundos*1000);</script>");
 }
 
 function setProgressOf($i, $total) {
     setProgress(percentOf($i, $total));
-}
-
-function getLinesNumber($file) {
-    $linecount = 0;
-    $handle    = fopen($file, "r");
-    while (!feof($handle)) {
-        if (fgets($handle) !== false) {
-            $linecount++;
-        }
-    }
-    fclose($handle);
-    return $linecount;
 }
