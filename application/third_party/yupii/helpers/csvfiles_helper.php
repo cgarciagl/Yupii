9357915d21d01c11fc86a10d6e6d5b5b2ad57727
Carlos García Trujillo <cgarciagl@gmail.com>
2019-02-01 14:19:31 -0600

    ajustes al helper de archivos CSV

--- application/third_party/yupii/helpers/csvfiles_helper.php
+++ application/third_party/yupii/helpers/csvfiles_helper.php
@@ -1,29 +1,34 @@
 <?php
 
-function arrayToCSVFile(array $results, $fileName = 'temp.csv') {
+function arrayToCSVFile(array $results, $fileName = 'temp.csv', $download = TRUE) {
 
-    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-    header('Content-Description: File Transfer');
-    header("Content-type: text/csv");
-    header("Content-Disposition: attachment; filename={$fileName}");
-    header("Expires: 0");
-    header("Pragma: public");
+    if ($download) {
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header('Content-Description: File Transfer');
+        header("Content-type: text/csv");
+        header("Content-Disposition: attachment; filename={$fileName}");
+        header("Expires: 0");
+        header("Pragma: public");
 
-    $fh = @fopen('php://output', 'w');
-
-    $headerDisplayed = false;
-
-    foreach ($results as $data) {
-        // Add a header row if it hasn't been added yet
-        if (!$headerDisplayed) {
-            // Use the keys from $data as the titles
-            fputcsv($fh, array_keys($data));
-            $headerDisplayed = true;
-        }
-
-        // Put the data into the stream
-        fputcsv($fh, $data);
+        $fh = @fopen('php://output', 'w');
     }
-    fclose($fh);
-    exit;
+    else {
+       $fh = @fopen('./temp.csv', 'w'); 
+   }
+
+   $headerDisplayed = false;
+
+   foreach ($results as $data) {
+    if (!$headerDisplayed) {
+        fputcsv($fh, array_keys($data));
+        $headerDisplayed = true;
+    }
+    fputcsv($fh, $data);
+}
+fclose($fh);
+
+if (!$download) {
+    rename('./temp.csv', $fileName);
+}
+
 }
